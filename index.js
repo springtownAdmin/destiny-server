161263fd841e62dd5c1add2561ac2e9b53944246
nikhil-springtown <nikhil@springtown.ai>
2024-11-23 08:32:09 +0000

    replace-send-with-json

--- index.js
+++ index.js
@@ -340,7 +340,7 @@ app.post('/api/logs', (req, res) => {
     }
     // siteLogger.log({ level, message });
 
-    res.status(200).send('Log received');
+    res.status(200).json({ message: 'Log received' });
 
 });
 
